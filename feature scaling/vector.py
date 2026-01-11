12609299580a8a3edabb154857080f84847c5df1
Vishal mourya <vm609356@gmail.com>
2026-01-11 18:31:10 +0530

    update 1

--- feature scaling/vector.py
+++ feature scaling/vector.py
@@ -5,3 +5,4 @@ import pandas as pd
 df=sns.load_dataset('iris')
 df1=pd.DataFrame(normalize(df[['sepal_length','sepal_width','petal_length','petal_width']]),columns=['sepal_length','sepal_width','petal_length','petal_width'])
 print(df1)
+
